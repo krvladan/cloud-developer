bf6a1b69bce19dec76503c9cefcde90865b09f95
Vladan Krstic <vladan.krstic@tomtom.com>
2020-06-27 18:36:51 +0200

    Get signed URL to upload image

--- course-04/exercises/lesson-3/starter-code/src/lambda/http/createImage.ts
+++ course-04/exercises/lesson-3/starter-code/src/lambda/http/createImage.ts
@@ -5,8 +5,14 @@ import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
@@ -25,15 +31,41 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     }
   }
 
+  const imageId = uuid.v4()
+  const newItem = await createImage(groupId, imageId, event)
+
+  const url = getUploadUrl(imageId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem: newItem,
+      uploadUrl: url
+    })
+  }
+}
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
+}
+
+async function createImage(groupId: string, imageId: string, event: APIGatewayProxyEvent) {
   const parsedBody = JSON.parse(event.body)
   const timestamp = new Date().toISOString()
-  const imageId = uuid.v4()
 
   const newItem = {
     groupId,
     timestamp,
     imageId,
-    ...parsedBody
+    ...parsedBody,
+    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
   }
   console.log('Storing new item: ', newItem)
 
@@ -42,15 +74,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     Item: newItem
   }).promise()
 
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      newItem
-    })
-  }
+  return newItem
 }
 
 async function groupExists(groupId: string) {
